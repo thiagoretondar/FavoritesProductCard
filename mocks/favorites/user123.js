bf2acdbaf129f1d9c31eec8225803ba7c571fa14
Thiago Retondar <t.retondar@gmail.com>
2017-04-02 21:48:28 -0300

    getting info from json

---
+++ mocks/favorites/user123.js
@@ -0,0 +1,26 @@
+export default {
+    product: {
+        id: 610472,
+        name: 'Apple Macbook Pro MF840 Intel Core i5 2.7 GHz 8192 MB 256 GB',
+        url: 'http://www.buscape.com.br/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb',
+        thumbnails: [
+            'http://thumbs.buscape.com.br/notebook/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb_300x300-PU950a8_1.jpg',
+        ],
+        quantityStores: 4,
+        bestPrice: {
+            store: {
+                name: 'MasterTronic.com.br',
+                link: 'http://www.buscape.com.br/redirect_prod?pagina=1&pos=0&id=6424&prod_id=164955557&emp_id=913308&nc=17230708520170331130201&az=5cafaef32f5dc0f385a17bab5c1136b8&cn=80454264',
+            },
+            value: 6221.75,
+            installment: {
+                quantity: 12,
+                value: 615.11,
+            },
+        },
+    },
+    reviewer: {
+        name: 'Thiago Retondar',
+        review: 'esse notebook é exelente, ideal para qualquer atividade, desde as mais simples as mais complexas que envolvem programas...',
+    },
+};
--- src/components/favoriteProduct.jsx
+++ src/components/favoriteProduct.jsx
@@ -1,56 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import userMock from '../../mocks/favorites/user123';
 
-const FavoriteProduct = () =>
-    <div className="mainProduct">
-        <section className="productPreview">
-            <img
-                alt="Macbook Pro"
-                src="http://thumbs.buscape.com.br/notebook/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb_300x300-PU950a8_1.jpg"
-            />
-        </section>
-        <section className="productInfo">
-            <h3>Apple Macbook Pro MF840 Intel Core i5 2.7 GHz 8192 MB 256 GB</h3>
-            <span className="productInfoMoreStore">
-                <a href="http://www.buscape.com.br/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb">
-                    VEJA PREÇOS EM 4 LOJAS
-                </a>
-            </span>
+class FavoriteProduct extends Component {
 
-            <div className="productInfoBestPrice">
-                <h4>Melhor Preço</h4>
-                <a href="http://www.buscape.com.br/redirect_prod?pagina=1&pos=0&id=6424&prod_id=164955557&emp_id=913308&nc=17230708520170331130201&az=5cafaef32f5dc0f385a17bab5c1136b8&cn=80454264">
-                    <span className="productInfoInstallments">
-                        12x 615,11
+    constructor(props) {
+        super(props);
+
+        this.state = userMock;
+    }
+
+    render() {
+        const { product, reviewer } = this.state;
+
+        return (
+            <div className="mainProduct">
+                <section className="productPreview">
+                    <img
+                        alt={product.name}
+                        src={product.thumbnails[0]}
+                    />
+                </section>
+                <section className="productInfo">
+                    <h3>{product.name}</h3>
+                    <span className="productInfoMoreStore">
+                        <a href={product.url}>
+                            VEJA PREÇOS EM {product.quantityStores} LOJAS
+                        </a>
                     </span>
-                    <span className="productInfoMinorPrice">
-                        R$ 6.221,75
-                    </span>
-                    <span className="productInfoMinorPriceStore">
-                        MasterTronic.com.br
-                    </span>
-                </a>
+
+                    <div className="productInfoBestPrice">
+                        <h4>Melhor Preço</h4>
+                        <a href={product.bestPrice.store.link}>
+                            <span className="productInfoInstallments">
+                                {product.bestPrice.installment.quantity}x {product.bestPrice.installment.value}
+                            </span>
+                            <span className="productInfoMinorPrice">
+                                R$ {product.bestPrice.value}
+                            </span>
+                            <span className="productInfoMinorPriceStore">
+                                {product.bestPrice.store.name}
+                            </span>
+                        </a>
+                    </div>
+                </section>
+                <section className="productRandomComment">
+                    <div className="productReview">
+                        <img
+                            src="http://image.buscape.com/material/icons/ic_avatar_review.svg"
+                            alt="Reviewer"
+                        />
+                        <div className="review">
+                            <h4>{reviewer.name}</h4>
+                            <p>{reviewer.review}</p>
+                            <a href="http://www.buscape.com.br/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb#avaliacoes">
+                                LEIA A AVALIAÇÃO DO CONSUMIDOR
+                            </a>
+                        </div>
+                    </div>
+                </section>
             </div>
-        </section>
-        <section className="productRandomComment">
-            <div className="productReview">
-                <img
-                    src="http://image.buscape.com/material/icons/ic_avatar_review.svg"
-                    alt="Reviewer"
-                />
-                <div className="review">
-                    <h4>
-                        Thiago Retondar
-                    </h4>
-                    <p>
-                        esse notebook é exelente, ideal para qualquer atividade,
-                        desde as mais simples as mais complexas que envolvem programas...
-                    </p>
-                    <a href="http://www.buscape.com.br/apple-macbook-pro-mf840-intel-core-i5-2-7-ghz-8192-mb-256-gb#avaliacoes">
-                        LEIA A AVALIAÇÃO DO CONSUMIDOR
-                    </a>
-                </div>
-            </div>
-        </section>
-    </div>;
+        );
+    }
+}
 
 export default FavoriteProduct;
