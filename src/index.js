02729dcda7fcb161818eec798ca7d55e4d4d1c8c
Thiago Retondar <t.retondar@gmail.com>
2017-04-09 15:59:06 -0300

    added redux-promise middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import reducers from './reducers';
 import FavoriteProductCard from './components/favoriteProductCard/favoriteProductCard';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
